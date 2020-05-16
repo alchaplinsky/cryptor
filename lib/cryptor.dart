0373611e3d68222a6d36f4d4aafd917b8e51598c
Alex Chaplinsky <alchaplinsky@gmail.com>
2020-05-17 01:21:10 +0200

    Implement pbkdf2 and get rid of password_hash dependency

--- lib/cryptor.dart
+++ lib/cryptor.dart
@@ -4,8 +4,8 @@ import 'dart:convert';
 import 'dart:typed_data';
 import 'dart:math';
 import 'package:crypto/crypto.dart';
-import 'package:password_hash/password_hash.dart';
 import 'package:steel_crypt/steel_crypt.dart';
+import './pbkdf2.dart';
 
 class Cryptor {
   static const SALT_LENGTH = 64;
@@ -16,38 +16,37 @@ class Cryptor {
 
   /// Encrypts passed [cleartext] with key generated based on [password] argument
   String encrypt(String cleartext, String password) {
-    var salt = _generateSalt();
-    var iv = _generateIV();
-    var key = _pbkdf2(password, salt);
-    var cipher = AesCrypt(key, 'gcm');
-    String ciphertext = cipher.encrypt(cleartext, base64.encode(iv));
+    Uint8List salt = _generateSalt();
+    Uint8List iv = _generateIV();
+    String key = _pbkdf2(password, salt);
+    String ciphertext =
+        AesCrypt(key, 'gcm').encrypt(cleartext, base64.encode(iv));
     return base64.encode(Uint8List.fromList(
         _combineInputs(salt, iv, _generateTag(), ciphertext)));
   }
 
   /// Encrypts passed [cryptedtext] with key generated based on [password] argument
   String decrypt(String cryptedtext, String password) {
-    var bytes = base64.decode(cryptedtext);
-    var salt = bytes.sublist(0, SALT_LENGTH);
-    var iv = bytes.sublist(SALT_LENGTH, SALT_LENGTH + IV_LENGTH);
-    var ciphertext = base64.encode(
+    Uint8List bytes = base64.decode(cryptedtext);
+    Uint8List salt = bytes.sublist(0, SALT_LENGTH);
+    Uint8List iv = bytes.sublist(SALT_LENGTH, SALT_LENGTH + IV_LENGTH);
+    String ciphertext = base64.encode(
         bytes.sublist(SALT_LENGTH + IV_LENGTH + TAG_LENGTH, bytes.length));
-    var key = _pbkdf2(password, salt);
-    var cipher = AesCrypt(key, 'gcm');
-    return cipher.decrypt(ciphertext, base64.encode(iv));
+    String key = _pbkdf2(password, salt);
+    return AesCrypt(key, 'gcm').decrypt(ciphertext, base64.encode(iv));
   }
 
-  List<int> _combineInputs(salt, iv, tag, ciphertext) {
+  List<int> _combineInputs(
+      Uint8List salt, Uint8List iv, Uint8List tag, String ciphertext) {
     return salt + iv + tag + base64.decode(ciphertext);
   }
 
   /// Password Based Key Deriviation function
   String _pbkdf2(String password, Uint8List salt) {
-    var generator = new PBKDF2(hashAlgorithm: sha512);
+    PBKDF2 generator = new PBKDF2(sha512);
     Uint8List bytes = Uint8List.fromList(generator.generateKey(
         password, base64.encode(salt), KEY_ITERATIONS_COUNT, KEY_LENGTH));
-    String hash = String.fromCharCodes(bytes);
-    return hash;
+    return String.fromCharCodes(bytes);
   }
 
   /// Generates empty tag since actual tag can not be obtained from AesCrypt
@@ -67,9 +66,9 @@ class Cryptor {
 
   /// Generates a random byte sequence of given [length]
   List<int> _randomBytes(int length) {
-    var buffer = new Uint8List(length);
-    final range = new Random.secure();
-    for (var i = 0; i < length; i++) {
+    Uint8List buffer = new Uint8List(length);
+    Random range = new Random.secure();
+    for (int i = 0; i < length; i++) {
       buffer[i] = range.nextInt(256);
     }
     return buffer;
--- test/cryptor_test.dart
+++ test/cryptor_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:cryptor/cryptor.dart';
 
 void main() {
-  test('adds one to input values', () {
+  test('Encrypts and decrypts string', () {
     final password = 'password';
     final cryptor = Cryptor();
     String encrypted = cryptor.encrypt('data', password);
